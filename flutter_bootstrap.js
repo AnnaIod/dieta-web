e4dee973804c37c553342f9dfa14e3095f0250c1
AnnaIod <a.iodice11@virgilio.it>
2026-01-07 22:37:21 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3729576815"
+    serviceWorkerVersion: "2807071677"
   }
 });
