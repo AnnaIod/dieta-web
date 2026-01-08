0c8be5ee42763de345295617a4348df39662ebc9
AnnaIod <a.iodice11@virgilio.it>
2026-01-08 17:49:19 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "683444878547fe2cc963474eb7d830c3
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "a3d777ad2bbed7cd2ced85e4816b7eed",
+"flutter_bootstrap.js": "f9f037d358c7bd0d73c862ce2119ae16",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
